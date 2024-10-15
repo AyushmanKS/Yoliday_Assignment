640a22a47d7608b4677bcd409e1e50234e5a0ab9
AyushmanKS <ayushmanks1845@gmail.com>
2024-10-16 03:34:30 +0530

    create custom AppBar inside Portfolio page

--- lib/pages/portfolio/portfolio_page.dart
+++ lib/pages/portfolio/portfolio_page.dart
@@ -11,27 +11,62 @@ class PortfolioPage extends StatefulWidget {
 class _PortfolioPageState extends State<PortfolioPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Portfolio'),
-        actions: [
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 8.0),
-            child: SvgPicture.asset(
-              'assets/images/icons/ic_round-shopping-bag.svg',
-              width: 28, // Adjust the width as needed
-              height: 28, // Adjust the height as needed
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
+        appBar: PreferredSize(
+          preferredSize:
+              const Size.fromHeight(160.0), // Set the height of the AppBar
+          child: Container(
+            margin: const EdgeInsets.only(top: 60),
+            child: AppBar(
+              title: const Padding(
+                padding: EdgeInsets.symmetric(
+                    horizontal: 8.0), // Padding only for title
+                child: Text('Portfolio'),
+              ),
+              actions: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                  child: SvgPicture.asset(
+                    'assets/images/icons/ic_round-shopping-bag.svg',
+                    width: 28,
+                    height: 28,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 20, right: 20.0),
+                  child: SvgPicture.asset(
+                    'assets/images/icons/Vector.svg',
+                    width: 28,
+                    height: 28,
+                  ),
+                ),
+              ],
+              bottom: const TabBar(
+                //isScrollable: true,
+                indicatorColor: Colors.orange,
+                labelColor: Colors.orange,
+                unselectedLabelColor: Colors.black,
+                indicatorSize: TabBarIndicatorSize.label,
+                tabs: [
+                  Tab(text: 'Project'),
+                  Tab(text: 'Saved'),
+                  Tab(text: 'Shared'),
+                  Tab(text: 'Achievement'),
+                ],
+              ),
             ),
           ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 8.0),
-            child: SvgPicture.asset(
-              'assets/images/icons/Vector.svg',
-              width: 28, // Adjust the width as needed
-              height: 28, // Adjust the height as needed
-            ),
-          ),
-        ],
+        ),
+        body: const TabBarView(
+          children: [
+            Center(child: Text('Project Content')),
+            Center(child: Text('Saved Content')),
+            Center(child: Text('Shared Content')),
+            Center(child: Text('Achievement Content')),
+          ],
+        ),
       ),
     );
   }
